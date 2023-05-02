741956176d6e7c75c4858142f1938bc54eb80e14
Surabhi Mahale <surabhimahale12@gmail.com>
2023-05-02 09:21:41 +0530

    Update fibonnaci.cpp

--- fibonnaci.cpp
+++ fibonnaci.cpp
@@ -21,3 +21,31 @@ int main()
   }
   return 0;
 }
+
+//display upto n number
+// #include <iostream>
+// using namespace std;
+
+// void fibonacci(int t1, int t2, int n) {
+//     int nextTerm = t1 + t2;
+//     if (nextTerm > n) {
+//         return;
+//     }
+//     cout << nextTerm << ", ";
+//     fibonacci(t2, nextTerm, n);
+// }
+
+// int main() {
+//     int t1 = 0, t2 = 1, n;
+
+//     cout << "Enter a positive number: ";
+//     cin >> n;
+
+//     // displays the first two terms which is always 0 and 1
+//     cout << "Fibonacci Series: " << t1 << ", " << t2 << ", ";
+
+//     fibonacci(t1, t2, n);
+
+//     return 0;
+// }
+// In this implementation, the fibonacci function takes three integer parameters: the first two terms t1 and t2 of the sequenc
